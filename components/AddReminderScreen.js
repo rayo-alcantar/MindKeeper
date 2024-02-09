83e0ede1627b4faa16651a4efe030d8cb3e7bd51
ángel Alcantar <rayoalcantar@gmail.com>
2024-02-08 20:56:00 -0600

    añadí lo principal.

---
+++ components/AddReminderScreen.js
@@ -0,0 +1,114 @@
+﻿import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Notifications from 'expo-notifications';
+
+const AddReminderScreen = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const [notificationsCount, setNotificationsCount] = useState('');
+  const [interval, setInterval] = useState('');
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const saveReminder = async () => {
+    try {
+      const reminder = {
+        name,
+        description,
+        date: date.toISOString(),
+        notificationsCount: parseInt(notificationsCount, 10),
+        interval: parseInt(interval, 10),
+      };
+      
+      await AsyncStorage.setItem('reminder', JSON.stringify(reminder));
+      scheduleNotifications(reminder);
+      Alert.alert("Recordatorio guardado con éxito");
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error al guardar el recordatorio");
+    }
+  };
+
+  const scheduleNotifications = async (reminder) => {
+    for (let i = 0; i < reminder.notificationsCount; i++) {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: `Recordatorio: ${reminder.name}`,
+          body: reminder.description,
+        },
+        trigger: {
+          date: new Date(Date.now() + i * reminder.interval * 60000), // Configura el intervalo en minutos
+        },
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        placeholder="Nombre del Recordatorio"
+        value={name}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setDescription}
+        placeholder="Descripción (Opcional)"
+        multiline
+        numberOfLines={4}
+        value={description}
+      />
+      <Button title="Seleccionar Fecha y Hora" onPress={() => setShow(true)} />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="datetime"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      <TextInput
+        style={styles.input}
+        onChangeText={setNotificationsCount}
+        placeholder="Número de notificaciones"
+        keyboardType="numeric"
+        value={notificationsCount}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setInterval}
+        placeholder="Intervalo entre notificaciones (minutos)"
+        keyboardType="numeric"
+        value={interval}
+      />
+      <Button title="Guardar Recordatorio" onPress={saveReminder} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+  },
+});
+
+export default AddReminderScreen;
---
+++ components/MainScreen.js
@@ -0,0 +1,85 @@
+﻿// MainScreen.js
+import React from 'react';
+import { StyleSheet, View, Text, Pressable, ScrollView, Linking } from 'react-native';
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>MindKeeper</Text>
+      <Pressable 
+        style={styles.button} 
+        onPress={() => navigation.navigate('CrearRecordatorio')}
+        accessibilityLabel="Crear recordatorio"
+        accessibilityRole="button"
+      >
+        <Text style={styles.buttonText}>Crear Recordatorio</Text>
+      </Pressable>
+      
+      <Pressable 
+        style={styles.button} 
+        onPress={() => navigation.navigate('GestionarRecordatorios')}
+        accessibilityLabel="Gestionar recordatorios"
+        accessibilityRole="button"
+      >
+        <Text style={styles.buttonText}>Gestionar Recordatorios</Text>
+      </Pressable>
+      
+      <Pressable 
+        style={styles.button} 
+        onPress={async () => {
+          const url = 'https://paypal.me/rayoalcantar';
+          const supported = await Linking.canOpenURL(url);
+          if (supported) {
+            await Linking.openURL(url);
+          } else {
+            console.log("No se puede abrir la URL: " + url);
+          }
+        }}
+        accessibilityLabel="Donar al desarrollador"
+        accessibilityRole="button"
+      >
+        <Text style={styles.buttonText}>Donar al Desarrollador</Text>
+      </Pressable>
+      
+      <Pressable 
+        style={styles.button} 
+        onPress={() => navigation.navigate('Ajustes')}
+        accessibilityLabel="Ajustes"
+        accessibilityRole="button"
+      >
+        <Text style={styles.buttonText}>Ajustes</Text>
+      </Pressable>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0F4F8',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#1A1A1A', // Cambiado a un color oscuro
+    padding: 15,
+    borderRadius: 10,
+    width: '50%', // Ajustado al 50% del contenedor padre
+    marginBottom: 10,
+    alignItems: 'center',
+    alignSelf: 'center', // Asegura que el botón se centre en el contenedor
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default MainScreen;
